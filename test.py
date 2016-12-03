3f702d73401b3836f14d7cb3ad6b638acc0ff871
Eddie <fly2695103@163.com>
2016-12-03 22:26:57 +0800

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-def newfoo(arg1,arg2,*nkw,**kw):
-    print 'arg1 is:',arg1 , 'arg2 is:',arg2 for eachNKW in nkw:
-        print 'additional non-keyword arg:',eachNKW
-    for eachkw in kw.keys():
-        print "additional keyword arg {0}:{1}".format(eachKW,kw[eachKW])
